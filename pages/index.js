13e11e26c3a826dfc7c9cd8383a5993f777bcbe9
Volkan Arıslı <volkanarisli@gmail.com>
2022-01-29 22:02:04 +0300

    landing page added

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,17 @@
 
-import Image from 'next/image'
-import axios from 'axios'
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router'
+import router from 'next/router'
 const Home = () => {
 
 
-  const getProfile = async () => {
-    const data = await axios.post('/api/fetchProfileInfo', { code })
-    console.log(data)
-  }
-  // useEffect(() => {
-  // const getAccessToken = async () => {
-  //   const data = await axios.post('/api/fetchProfileInfo', { code })
-  //   console.log(data)
-  // }
-  //   const { query: { code } } = router
-  //   setCode(code)
-  //   if (!code) return
-  //   getAccessToken();
-  //   return () => {
 
-  //   }
-  // }, [router])
+  useEffect(() => {
+
+    router.push('/index.html')
+  }, [])
   return (
     <div>
-      <button onClick={getProfile}>Deneme</button>
+
     </div>
   )
 }
