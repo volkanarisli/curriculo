7ecfac3d57ece3871aad19b4d3b7bb1c29a2b974
Volkan Arıslı <volkanarisli@gmail.com>
2022-01-29 13:38:09 +0300

    Typo fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.scss'
 import Head from 'next/head'
-import ResumeInfoProvider from '../context/resumeInfo'
+import ResumeInfoProvider from '../context/ResumeInfo'
 
 
 const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
