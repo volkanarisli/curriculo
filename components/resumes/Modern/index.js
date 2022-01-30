00d600e440bf728ada6491cb08e89df1e798088a
Volkan Arıslı <volkanarisli@gmail.com>
2022-01-30 13:10:38 +0300

    modern cv template added

--- components/resumes/Modern/index.js
+++ components/resumes/Modern/index.js
@@ -2,6 +2,10 @@ import Bio from './components/Bio'
 import Education from './components/Education'
 import Experience from './components/Experience'
 import { useResumeInfo } from "../../../context/ResumeInfo";
+import gradientAlt from "../../../assets/img/gradientAlt.svg"
+import gradientUst from "../../../assets/img/gradientUst.svg"
+import Image from 'next/image';
+
 
 
 
@@ -10,7 +14,10 @@ const Modern = () => {
     const { allResumeData: { about, contact, educationHistory, experiences } } = useResumeInfo()
 
     return (
-        <div className="w-a4 min-h-a4 border bg-Modern-background font-roboto-mono p-10">
+        <div className="w-a4 min-h-a4 border bg-Modern-background font-roboto-mono p-10 relative overflow-hidden">
+            <div className="absolute -z-10 left-0 top-0 -ml-52 -mt-44">
+                <Image src={gradientUst} alt="gradient" height="700" width="700" />
+            </div>
             <div className="mb-10">
                 <Bio about={about} contact={contact} />
             </div>
@@ -21,8 +28,9 @@ const Modern = () => {
                 <Education educationHistory={educationHistory} />
 
             </div>
-
-
+            <div className="absolute -z-10 right-0 bottom-0 -mr-52 -mb-44">
+                <Image src={gradientAlt} alt="gradient" height="700" width="700" />
+            </div>
         </div>
 
     )
