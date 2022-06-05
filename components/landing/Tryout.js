83a562376f8c87cbc871b0bfa07fa9868b184475
Volkan Arıslı <volkanarisli@gmail.com>
2022-06-05 16:24:35 +0300

    eslint fix

--- components/landing/Tryout.js
+++ components/landing/Tryout.js
@@ -41,7 +41,7 @@ const Tryout = () => {
                 <span className="text-blue-700 font-semibold " id="demo">Demo</span>
                 <span className="text-gray-900 text-3xl font-semibold">See Curriculo In Action</span>
                 <p className="text-sm text-gray-500">
-                    This demo is a great way to see how Curriculo can help you create killer cover letters and resume texts! You can't change the inputs, but that just means you can focus on the awesome results. Plus, you can't include or exclude anything personal, so this demo is the perfect way to understand how the process works.
+                    This demo is a great way to see how Curriculo can help you create killer cover letters and resume texts! You can&apos;t change the inputs, but that just means you can focus on the awesome results. Plus, you can&apos;t include or exclude anything personal, so this demo is the perfect way to understand how the process works.
                 </p>
             </div>
             <div className="max-w-4xl mx-auto py-16 px-4 sm:px-6 lg:px-8" id="action">
