a7a10ff4397c50e295e0171554387a3572e73272
Volkan Arıslı <volkanarisli@gmail.com>
2022-07-16 22:15:38 +0300

    update

--- pages/blog/post/[slug].js
+++ pages/blog/post/[slug].js
@@ -2,17 +2,25 @@ import { getSingleBlogPost } from "../../../utils/notion"
 import ReactMarkdown from "react-markdown";
 import Head from "next/head";
 import styles from '../../../assets/styles/markdown.module.scss'
+import { useRouter } from "next/router";
 
 const Post = ({ markdown, post }) => {
+    const router = useRouter()
 
     return (
         <div className={styles.markdown}>
             <Head>
                 <title>{post.title}</title>
                 <meta name={"description"} title={"description"} content={post.description} />
-                <meta name={"og:title"} title={"og:title"} content={post.title} />
-                <meta name={"og:description"} title={"og:description"} content={post.description} />
-                <meta name={"og:image"} title={"og:image"} content={post.cover} />
+                <meta property="og:title" title={"og:title"} content={post.title} />
+                <meta property="og:description" title={"og:description"} content={post.description} />
+                <meta property="og:image" title={"og:image"} content={post.cover} />
+                <meta property="og:url" content={`https://www.curriculo.design${router.asPath}`} />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content={`https://www.curriculo.design${router.asPath}`} />
+                <meta property="twitter:title" content={post.title} />
+                <meta property="twitter:description" content={post.description} />
+                <meta property="twitter:image" content={post.cover} />
             </Head>
 
             <div className="min-h-screen">
